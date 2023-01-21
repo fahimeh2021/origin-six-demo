5cdfe122873ae4e3b27d0c58a3bce384d22dd22f
fahimeh2021 <95322523+fahimeh2021@users.noreply.github.com>
2023-01-20 20:27:46 -0800

    add menu

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ const mobileMenu = document.getElementById("mobile-menu");
 const hamburgerButton = document.getElementById("hamburger-btn");
 hamburgerButton.addEventListener("click", showMobileMenu);
 function showMobileMenu() {
-    mobileMenu.style.display = "block";
+  mobileMenu.style.display = "block";
+}
+const mobileX = document.getElementById("times");
+mobileX.addEventListener("click", closemenu);
+function closemenu() {
+  mobileMenu.style.display = "none";
 }
